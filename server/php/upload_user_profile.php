05e31b6d166e0eb1857aee1c6bba057b597ac3c8
nodeal <nodealdev@gmail.com>
2020-08-14 16:11:39 +0900

    c

--- server/php/upload_user_profile.php
+++ server/php/upload_user_profile.php
@@ -1,6 +1,6 @@
 <?php
 $user_id = $_GET['user_id'];
-$target_path = "/var/www/html/raw/".$user_id."/profile.png";
+$target_path = "/var/www/html/raw/user/".$user_id."/profile.png";
 
 if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_path)) {
     echo(json_encode(array(
